b7e3cd52f008d8497d23e243b3902473dd87085c
Muhammad Amirxon <muhammadamirxon14@gmail.com>
2024-09-07 15:57:50 +0500

    ishni boshladim

--- my-app/src/pages/page1.jsx
+++ my-app/src/pages/page1.jsx
@@ -1,10 +1,17 @@
 import './page1.css'
-
-
+import logo from './foto/logo'
+import foto1 from './foto/foto1'
+import foto2 from './foto/foto2'
 
 function Page1(){
   return (
     <div className="Page1">
+
+        <div>
+
+        <img src={logo} alt="" />
+
+        </div>
       
     </div>
   );
