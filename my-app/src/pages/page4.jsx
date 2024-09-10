0e2b6e4bf73d31df9f2c4e86cf61c3531397df0c
Muhammad Amirxon <muhammadamirxon14@gmail.com>
2024-09-10 13:04:59 +0500

    css qo'shdim

--- my-app/src/pages/page4.jsx
+++ my-app/src/pages/page4.jsx
@@ -1,12 +1,80 @@
 import './page4.css'
-
-
+import p1 from '../foto/p1.jpg'
+import p2 from '../foto/p2.jpg'
+import p3 from '../foto/p3.jpg'
+import p4 from '../foto/p4.jpg'
 
 function Page4(){
   return (
     <div className="Page4">
 
-      
+      <div className="gh">
+
+        <div className="ff">
+
+          <h1>Our Services</h1>
+
+          <h1 className='q'>Perfect and Fast<br></br>
+Movement</h1>
+
+          <h1 className='w'>We move with make a Creative Strategy for help your business<br></br> goal, we help to improve your income by a services we have.<br></br> make your content look interesting and make people look for<br></br> your business</h1>
+
+        </div>
+
+
+        <div className="gg">
+
+
+          <div className="r1">
+
+            <div className="yu">
+
+              <img src={p1} alt="" />
+
+              <h3>Social Media<br></br>
+Management</h3>
+
+            </div>
+
+            <div className="yu">
+
+              <img src={p1} alt="" />
+
+              <h3>Social Media<br></br>
+Management</h3>
+
+            </div>
+
+          </div>
+
+
+          <div className="r2">
+
+          <div className="yu">
+
+<img src={p1} alt="" />
+
+<h3>Social Media<br></br>
+Management</h3>
+
+</div>
+
+<div className="yu">
+
+              <img src={p1} alt="" />
+
+              <h3>Social Media<br></br>
+Management</h3>
+
+            </div>
+
+          </div>
+
+
+        </div>
+
+
+      </div>
       
     </div>
   );
